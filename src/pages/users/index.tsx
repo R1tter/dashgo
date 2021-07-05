5ef2360414a9423f132a3bf8e078cdc5a0c5ce05
Marcelo Ritter <marcelosori@hotmail.com>
2021-07-05 02:57:19 -0300

    Feat: User component

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,141 @@
+import { 
+  Box, 
+  Flex, 
+  Heading, 
+  Button, 
+  Icon, 
+  Table,
+  Thead,  
+  Th,
+  Checkbox, 
+  Tbody,
+  Tr,
+  Td,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
+import { Header } from '../../components/Header';
+import  { Sidebar } from '../../components/Sidebar';
+
+export default function UserList(){
+  return(
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha" >
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>
+                  Usuário
+                </Th>
+                <Th>
+                  Data de cadastro
+                </Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Marcelo Ritter</Text>
+                    <Text fontSize="sm" color="gray.300">marcelosori@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  05 de Julho, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Marcelo Ritter</Text>
+                    <Text fontSize="sm" color="gray.300">marcelosori@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  05 de Julho, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Marcelo Ritter</Text>
+                    <Text fontSize="sm" color="gray.300">marcelosori@hotmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  05 de Julho, 2021
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16"/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}

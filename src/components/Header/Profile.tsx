a96d0ba2eddde22412830abc0662a9d01cf331b2
Marcelo Ritter <marcelosori@hotmail.com>
2021-07-07 01:11:05 -0300

    Feat: Header responsive.

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Flex, Text, Box, Avatar} from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
+      { showProfileData && (<Box mr="4" textAlign="right">
         <Text>Marcelo Ritter</Text>
         <Text color="gray.300" fontSize="small">
           marcelosori@hotmail.com
         </Text>
       </Box>
+      )}
 
       <Avatar size="md" name="Marcelo Ritter" src="https://github.com/R1tter.png" />
     </Flex>

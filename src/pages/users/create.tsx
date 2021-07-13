71b9e62df20200914a90e8061e8b7e3570ef3334
Marcelo Ritter <marcelosori@hotmail.com>
2021-07-13 11:45:56 -0300

    Feat: Navigation Flux

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   SimpleGrid,
   VStack,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 
 
@@ -41,7 +42,9 @@ export default function CreateUser(){
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+            <Link href="/users" passHref>
+              <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+            </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
@@ -36,16 +37,17 @@ export default function UserList(){
         <Box flex="1" borderRadius={8} bg="gray.800" p="8" overflowX="auto">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
-
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
           <Table colorScheme="whiteAlpha" >
             <Thead>

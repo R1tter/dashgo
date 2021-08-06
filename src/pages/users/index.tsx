ba3faf1e41d36ca30660008980b1ac3bdc6204bc
Marcelo Ritter <marcelosori@hotmail.com>
2021-08-06 17:48:07 -0300

    Feat: pagination done.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,7 +16,7 @@ import {
   Spinner,
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RiAddLine } from 'react-icons/ri';
 
 import { Header } from '../../components/Header';
@@ -25,17 +25,14 @@ import { Sidebar } from '../../components/Sidebar';
 import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, isFetching, error } = useUsers()
+  const [ page,  setPage ] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page)
 
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
-  useEffect(() => {
-
-  }, [])
 
   return (
     <Box>
@@ -91,7 +88,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map(user => {
+                  {data.users.map(user => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -113,9 +110,9 @@ export default function UserList() {
               </Table>
 
               <Pagination
-                totalCountOfRegisters={200}
-                currentPage={5}
-                onPageChange={() => {}}
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
               />
             </>
           )}

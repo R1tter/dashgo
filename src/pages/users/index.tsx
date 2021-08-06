d8ad7c29d08f457297b49118ad1af03f75e0a6b7
Marcelo Ritter <marcelosori@hotmail.com>
2021-08-06 15:51:47 -0300

    Feat: pagination Content

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -112,7 +112,11 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={5}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>

5dc1bdeecf6c0069a5bc740e1785d0e507f02c30
Marcelo Ritter <marcelosori@hotmail.com>
2021-08-24 16:15:18 -0300

    Feat: Drawer itens

---
+++ src/pages/automation/index.tsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function Automation() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8" overflowX="auto">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading
+              size="lg"
+              fontWeight="normal">
+                Automação : PlaceHolder
+            </Heading>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
